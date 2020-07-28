a3dab5d3589493f0059e0a5cdacf0b0595978839
Nightwalker <iniyan19@gmail.com>
2020-07-28 21:25:22 +0530

    Add files via upload

---
+++ Day-7/Day-7_Assignment_2.py
@@ -0,0 +1,3 @@
+list1=[(1,2),(3,4),(5,6),(4,5)]
+list2 =[sum(i) for i in list1]
+print(list2)
---
+++ Day-7/Day-7_Assignment_3.py
@@ -0,0 +1,3 @@
+list1=[(1,2,3),[1,2],['a','hit','less']]
+list2 = [i for each in list1 for i in each ]
+print ("The output:",list2)
